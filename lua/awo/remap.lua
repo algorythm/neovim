9d5fdf5bc2ba96b298347252ef127c9900d7ca07
Anders Wiberg Olsen <anders@wiberg.tech>
2025-06-06 08:14:31 +0200

    Remap `Ex` to `<Space>.`

--- lua/awo/remap.lua
+++ lua/awo/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>.", vim.cmd.Ex)
 
 vim.keymap.set("v", ">", ">gv")
 vim.keymap.set("v", "<", "<gv")
